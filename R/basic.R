dcd1ef20796dd931d896c19a98878b3568bc722f
seokhoonj <seokhoonj@gmail.com>
2021-02-03 18:14:40 +0900

    round -> floor

--- R/basic.R
+++ R/basic.R
@@ -7,15 +7,20 @@ lsjoin <- function(..., by, all = FALSE, all.x = all, all.y = all, sort = TRUE) 
   l <- list(...)
   Reduce(function(...) merge(..., by = by, all = all, all.x = all.x, all.y = all.y, sort = sort), l)
 }
-strati <- function(data, var, n, method, verbose) {
+strati <- function(data, var, size, method, verbose) {
   var <- vapply(substitute(var), deparse, FUN.VALUE = "character")
   var <- names(data)[match(var, names(data), 0L)]
   if (missing(method)) method <- "srswor"
   if (missing(verbose)) verbose <- TRUE
   grp <- data[, .(n = .N), by = var]
-  low <- min(grp$n)
-  grp[, s := min(low, n)]
-  grp[, p := s/n]
+  if (size < 1) {
+    grp[, s := floor(n*size)]
+    grp[, p := s/n]
+  } else {
+    low <- min(grp$n)
+    grp[, s := min(low, size)]
+    grp[, p := s/n]
+  }
   if (verbose) print(grp)
   ss <- strata(data = data, stratanames = var, size = grp$s, method = method)
   getdata(data, ss)
