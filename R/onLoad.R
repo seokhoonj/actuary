bd569616b692546bbed5a6b4e77be40a1e239c5a
seokhoonj <seokhoonj@gmail.com>
2021-02-08 11:21:53 +0900

    theme variables are added

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,17 +1,24 @@
-actuary.theme <- function(strip.size, x.size, x.angle) {
-  theme(
-    text = element_text(family = "Malgun Gothic"),
-    title = element_text(family = "Malgun Gothic"),
-    strip.text.x = element_text(size = 11, face = "bold"),
-    axis.text.x = element_text(size = 11, face = "bold"),
-    axis.text.y = element_text(size = 11, face = "bold"),
-    legend.position = "bottom"
-  )
-}
+.view.theme <- theme(
+  text = element_text(family = "Malgun Gothic"),
+  title = element_text(family = "Malgun Gothic"),
+  strip.text.x = element_text(face = "bold"),
+  axis.text.x = element_text(face = "bold", angle = 90),
+  axis.text.y = element_text(face = "bold"),
+  legend.position = "bottom"
+)
+.save.theme <- theme(
+  text = element_text(family = "Malgun Gothic"),
+  title = element_text(family = "Malgun Gothic"),
+  strip.text.x = element_text(size = 17, face = "bold"),
+  axis.text.x = element_text(size = 12, face = "bold", angle = 90),
+  axis.text.y = element_text(size = 12, face = "bold"),
+  legend.position = "bottom"
+)
 
 .onLoad = function(libname, pkgname) {
   opts = c("actuary.eps" = "1e-8",
-           "actuary.theme" = actuary.theme)
+           "view.theme" = .view.theme,
+           "save.theme" = .save.theme)
   for (i in setdiff(names(opts),names(options()))) {
     eval(parse(text=paste0("options(", i ,"=", opts[i], ")")))
   }
