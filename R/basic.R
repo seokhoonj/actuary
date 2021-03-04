da03411b0997d7c4b48a083bdc0fe6eaa24a0989
seokhoonj <seokhoonj@gmail.com>
2021-03-04 14:08:32 +0900

    method variable is removed in strati function

--- R/basic.R
+++ R/basic.R
@@ -109,11 +109,10 @@ join <- function(..., by, all = FALSE, all.x = all, all.y = all, sort = TRUE) {
   l <- list(...)
   Reduce(function(...) merge(..., by = by, all = all, all.x = all.x, all.y = all.y, sort = sort), l)
 }
-strati <- function(data, var, size, method, verbose) {
+strati <- function(data, var, size, verbose) {
+  if (missing(verbose)) verbose <- TRUE
   var <- vapply(substitute(var), deparse, FUN.VALUE = "character")
   var <- names(data)[match(var, names(data), 0L)]
-  if (missing(method)) method <- "srswor"
-  if (missing(verbose)) verbose <- TRUE
   grp <- data[, .(n = .N), by = var]
   grp[, g := 1:nrow(grp)]
   if (size < 1) {
