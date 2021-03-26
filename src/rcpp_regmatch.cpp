d23f4672f5ed407cd904add9987321b148ba61c9
seokhoonj <seokhoonj@gmail.com>
2021-03-26 13:17:12 +0900

    numeric i -> const auto &v in rcpp_regmatch function

--- src/rcpp_regmatch.cpp
+++ src/rcpp_regmatch.cpp
@@ -9,11 +9,13 @@ StringVector regmatch(std::string m, std::vector<std::string>& x, std::string de
   std::string s;
   std::vector<std::string> ss;
   std::vector<std::string> res;
-  for (size_t i = 0; i < x.size(); ++i) {
+  // for (size_t i = 0; i < x.size(); ++i) {
+  for (const auto &v : x) {
     res.clear();
     std::regex re(m);
     std::smatch match;
-    auto start = std::sregex_iterator(x[i].begin(), x[i].end(), re);
+    // auto start = std::sregex_iterator(x[i].begin(), x[i].end(), re);
+    auto start = std::sregex_iterator(v.begin(), v.end(), re);
     auto end = std::sregex_iterator();
     while (start != end) {
       res.push_back(start->str());
