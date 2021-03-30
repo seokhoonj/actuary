1cd0c3416156fe787e8e00f2b32e8f4b69ba1e3f
seokhoonj <seokhoonj@gmail.com>
2021-03-30 09:10:15 +0900

    regmatch function is unexported

--- R/basic.R
+++ R/basic.R
@@ -2,9 +2,6 @@
 # basic functions ---------------------------------------------------------
 
 unilen <- function(x) length(unique(x))
-# regmatch <- function(m, x, delim = "|") {
-#   rcpp_regmatch(m, x, delim)
-# }
 spl_prm_cnt <- function(x, n) rcpp_spl_prm_cnt(x, n)
 rep.data.table <- function(x, ...) as.data.table(lapply(x, rep, ...))
 get_class <- function(x) {
